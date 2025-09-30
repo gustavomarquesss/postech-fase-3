ebbcc1f35f884408ddeb858a680e4b6e340c3b18
Thomaz Lucca <thomazlucca@gmail.com>
2025-09-30 09:53:18 -0300

    removendo preview

--- vite.config.ts
+++ vite.config.ts
@@ -15,17 +15,4 @@ export default defineConfig({
       },
     },
   },
-  preview: {
-    port: 5000,
-    cors: true,
-    allowedHosts: ["postech-fase-3.onrender.com/"],
-    proxy: {
-      "/api": {
-        target: "https://fiap-fsdt-techchallenge-ii-posts.onrender.com",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-        secure: true,
-      },
-    },
-  },
 });
